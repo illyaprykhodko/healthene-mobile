a026443c5b48b258b91d42d56d6ea6118cdb2353
Viktor <s.vik.json@gmail.com>
2025-07-30 19:30:48 +0300

    HS-2576/ Public screens -Refactoring app component

--- App.tsx
+++ App.tsx
@@ -6,52 +6,40 @@
  */
 // outsource dependencies
 import { Provider } from 'react-redux';
-import React, { useEffect, useState } from 'react';
+// import './ReactotronConfig';
+import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 // local dependencies
-import { SplashScreen } from './src/components/SplashScreen';
+import { store } from './src/store';
 import { ThemeProvider } from './src/providers/ThemeProvider';
 import { RootNavigator } from './src/navigation/RootNavigator';
-import { store, initApp, checkAppHealth, restoreAppSession, useAppDispatch } from './src/store';
+import { useAppInitialization } from './src/hooks/useAppInitialization';
 
-function AppContent(): React.JSX.Element {
-  const dispatch = useAppDispatch();
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Initialize app
-    dispatch(initApp());
-    dispatch(checkAppHealth());
-    dispatch(restoreAppSession());
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 5000);
-  }, [dispatch]);
-
-  const handleSplashFinish = () => {
-    setIsLoading(false);
-  };
-
-  if (isLoading) {
-    return <SplashScreen onFinish={handleSplashFinish} />;
-  }
-
-  return <RootNavigator />;
+import { config } from './src/constants';
+import { BoxHolder, MaintenanceHolder } from 'components/preloader';
+if (config.DEBUG) {
+    require('./ReactotronConfig');
+}
+function AppContent (): React.JSX.Element {
+    const { isInitializing, health, healthError } = useAppInitialization();
+    if (isInitializing) { return <BoxHolder active />; }
+    if (!health) { return <MaintenanceHolder active />; }
+    return <RootNavigator />;
 }
 
-function App(): React.JSX.Element {
-  return (
-    <Provider store={store}>
-      <SafeAreaProvider>
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <ThemeProvider>
-          <AppContent />
-          </ThemeProvider>
-        </GestureHandlerRootView>
-      </SafeAreaProvider>
-    </Provider>
-  );
+function App (): React.JSX.Element {
+    return (
+        <Provider store={store}>
+            <SafeAreaProvider>
+                <GestureHandlerRootView style={{ flex: 1 }}>
+                    <ThemeProvider>
+                        <AppContent />
+                    </ThemeProvider>
+                </GestureHandlerRootView>
+            </SafeAreaProvider>
+        </Provider>
+    );
 }
 
 export default App;
