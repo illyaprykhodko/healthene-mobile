16f7b88f0e5eb27d1a8766c142eeed0b1a592f41
Alexandr <ua.oleksandr.feshchenko@gmail.com>
2025-10-22 16:34:54 +0300

    enhancement/HS-2666 Update android app - Clean code

--- react-native.config.js
+++ react-native.config.js
@@ -7,13 +7,4 @@ module.exports = {
         './assets/fonts/',
         './assets/icomoon/fonts/',
     ],
-    // dependencies: {
-    //     'react-native-video': {
-    //         platforms: {
-    //             android: {
-    //                 sourceDir: '../node_modules/react-native-video/android-exoplayer',
-    //             },
-    //         },
-    //     },
-    // },
 };
