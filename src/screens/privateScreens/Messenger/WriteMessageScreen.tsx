35c893657834f802a89ab40c975d9421ea045a48
Alexandr <ua.oleksandr.feshchenko@gmail.com>
2025-12-08 16:21:10 +0200

    Create action buttons

--- src/screens/privateScreens/Messenger/WriteMessageScreen.tsx
+++ src/screens/privateScreens/Messenger/WriteMessageScreen.tsx
@@ -4,11 +4,13 @@ import { Formik } from 'formik';
 import { useSelector } from 'react-redux';
 import React, { useCallback } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { ScrollView, StyleSheet, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { Pressable, ScrollView, StyleSheet, View } from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 // local dependencies
 import Text from 'components/Text.tsx';
+import { filters } from 'services/filter';
 import Screen from 'components/Screen.tsx';
 import { useTheme } from 'hooks/useTheme.ts';
 import { OFFSET } from 'constants/offset.ts';
@@ -21,6 +23,12 @@ import { RootStackParamList } from 'services/navigation';
 import { useCreateChainMutation, useReplyToChainMutation } from 'store/api/messengerApi.ts';
 
 // configure
+const ATTACHMENTS = {
+    VIDEO: 'VIDEO',
+    AUDIO: 'AUDIO',
+    FILE: 'FILE',
+};
+type AttachmentType = typeof ATTACHMENTS[keyof typeof ATTACHMENTS];
 const validationSchema = yup.object().shape({
     subject: yup.string()
         .trim()
@@ -48,6 +56,29 @@ const WriteMessageScreen = () => {
         }
         navigation.goBack();
     }, [chain, navigation]);
+
+    const getAttachment = useCallback((item: AttachmentType) => {
+        switch (item) {
+            default: return <Pressable key={item} style={[styles.mediaButton, { backgroundColor: theme.colors.lightGrey }]}>
+                <View style={[styles.mediaButtonIcon, { backgroundColor: theme.colors.lighterGrey }]}>
+                    <Icon name="video" color={theme.colors.darkGrey} size={20} />
+                </View>
+                <Text color={theme.colors.darkGrey}>{filters.humanize(item)}</Text>
+            </Pressable>;
+            case ATTACHMENTS.AUDIO: return <Pressable key={item} style={[styles.mediaButton, { backgroundColor: theme.colors.lightGrey }]}>
+                <View style={[styles.mediaButtonIcon, { backgroundColor: theme.colors.lighterGrey }]}>
+                    <Icon name="microphone" color={theme.colors.darkGrey} size={20} />
+                </View>
+                <Text color={theme.colors.darkGrey}>{filters.humanize(item)}</Text>
+            </Pressable>;
+            case ATTACHMENTS.FILE: return <Pressable key={item} style={[styles.mediaButton, { backgroundColor: theme.colors.lightGrey }]}>
+                <View style={[styles.mediaButtonIcon, { backgroundColor: theme.colors.lighterGrey }]}>
+                    <Icon name="paperclip" color={theme.colors.darkGrey} size={20} />
+                </View>
+                <Text color={theme.colors.darkGrey}>{filters.humanize(item)}</Text>
+            </Pressable>;
+        }
+    }, []);
 
     return <Screen initialized={true} style={styles.container}>
         <ScrollView>
@@ -95,6 +126,9 @@ const WriteMessageScreen = () => {
                             onChangeText={handleChange('text')}
                             error={touched.text && errors.text ? { text: errors.text } : undefined}
                         />
+                        <View style={styles.attachmentsContainer}>
+                            {Object.values(ATTACHMENTS).map(item => getAttachment(item))}
+                        </View>
                         <Button
                             variant="outline"
                             title="SEND MESSAGE"
@@ -120,6 +154,26 @@ const styles = StyleSheet.create({
     },
     formContainer: {
         marginVertical: OFFSET.VERTICAL,
+    },
+    attachmentsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginVertical: OFFSET.VERTICAL
+    },
+    mediaButtonIcon: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: OFFSET.POINT
+    },
+    mediaButton: {
+        width: '30%',
+        alignItems: 'center',
+        padding: 20,
+        justifyContent: 'space-around',
+        borderRadius: 5,
     },
     row: {
         flexDirection: 'row'
