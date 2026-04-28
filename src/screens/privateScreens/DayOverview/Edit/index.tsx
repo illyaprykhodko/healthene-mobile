3941ac67e057e027ef3684d1d4bbc218559e5063
Viktor <s.vik.json@gmail.com>
2026-04-28 10:19:17 +0300

    enhancement/HS-2955 Minor issues - Connect phase line - Adjust change phase behavior - Update rating handler

--- src/screens/privateScreens/DayOverview/Edit/index.tsx
+++ src/screens/privateScreens/DayOverview/Edit/index.tsx
@@ -36,7 +36,6 @@ import { ListItemSkeleton, Skeleton } from 'components/Skeleton';
 import ReplaceItemModal from 'components/modals/ReplaceItemModal';
 import SwipeList, { SwipeValueChange } from 'components/SwipeList';
 import ConfirmationReplaceModal from 'components/modals/ConfirmationReplaceModal';
-import { useUpdatePatientGamblingPointsMutation } from 'store/api/gamblingPointsApi';
 import { selectDayOverview, addRecentlyCompletedPhase } from 'store/slices/dayOverviewSlice';
 import { OVERVIEW_TYPE, ENTITY_TYPE, SECTION, PHASE_ITEM_STATUS, SUBSTANCE_TYPE } from 'constants/spec';
 
